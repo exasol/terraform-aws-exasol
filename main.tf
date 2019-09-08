a7289c213145fb43ac5d7f5eede14c9c0b67bfe6
Muhammet Orazov <m.orazow@gmail.com>
2019-09-08 21:54:00 +0200

    Prepare for public release (#1)
    * Update readme with usage example. Add descriptions to inputs.
    * Adds .travis.yml.
    * Update cloudformation template url.

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 data "aws_ami" "exasol" {
   most_recent = true
-  owners = ["${var.ami_image_owner}"]
+  owners      = ["${var.ami_image_owner}"]
 
   filter {
     name   = "name"
@@ -10,24 +10,28 @@ data "aws_ami" "exasol" {
 }
 
 resource "aws_cloudformation_stack" "exasol_cluster" {
-  name          = var.cluster_name
-  capabilities  = ["CAPABILITY_IAM"]
-  on_failure    = "DELETE"
-  template_url  = "https://mo-test-cf-templates.s3.eu-central-1.amazonaws.com/cloudformation_template_v0.2.0.yml"
+  name         = var.cluster_name
+  capabilities = ["CAPABILITY_IAM"]
+  on_failure   = "DELETE"
+  template_url = "https://exasol-cf-templates.s3.eu-central-1.amazonaws.com/cloudformation_template_v0.0.1.yml"
 
   parameters = {
     DBSystemName              = var.database_name
     DBPassword                = var.sys_user_password
     ExasolPassword            = var.admin_user_password
     SubnetId                  = var.subnet_id
-    DBSecurityGroup           = var.security_group
+    DBSecurityGroup           = var.security_group_id
     LicenseServerInstanceType = var.management_server_instance_type
     DatabaseNodeInstanceType  = var.datanode_instance_type
     DBNodeCount               = var.datanode_count
     StandbyNode               = var.standbynode_count
     KeyName                   = var.key_pair_name
     ImageId                   = var.ami_image_id == null ? var.ami_image_id : data.aws_ami.exasol.id
-    PublicIP                  = var.public_ips
+    PublicIP                  = var.public_ip
+    CreateS3Bucket            = var.create_s3_bucket
+    CreateS3Endpoint          = var.create_s3_endpoint
+    CreateKMSEndpoint         = var.create_kms_endpoint
+    CreateEC2Endpoint         = var.create_ec2_endpoint
   }
 
   tags = {
---
+++ scripts/ci.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -o errtrace -o nounset -o pipefail -o errexit
+
+# Goto parent (base) directory of this script.
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+cd "$BASE_DIR"
+
+REGION='eu-central-1'
+export AWS_REGION=${REGION}
+
+run_terraform_init () {
+  terraform init
+}
+
+run_terraform_fmt () {
+    if ! terraform fmt -check=true .
+    then
+      echo "terraform fmt failed!"
+      terraform fmt -check=true -diff .
+      exit 1
+    fi
+}
+
+run_terraform_validate () {
+  terraform validate -var "region=${AWS_REGION}"
+}
+
+run_tflint () {
+  tflint .
+}
+
+run_clean_worktree_check () {
+  # To be executed after all other steps, to ensures that there is no
+  # uncommitted code and there are no untracked files, which means
+  # .gitignore is complete and all code is part of a reviewable commit.
+  GIT_STATUS="$(git status --porcelain)"
+  if [[ $GIT_STATUS ]]; then
+    echo "Your worktree is not clean,"
+    echo "there is either uncommitted code or there are untracked files:"
+    echo "${GIT_STATUS}"
+    exit 1
+  fi
+}
+
+run_terraform_init
+run_terraform_fmt
+run_terraform_validate
+run_tflint
+run_clean_worktree_check
