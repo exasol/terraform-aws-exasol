cb3b67012b00ce18b33833034314ce5d839ce3a4
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-06-26 14:03:11 +0200

    Specify versions for terraform modules

--- versions.tf
+++ versions.tf
@@ -1,4 +1,14 @@
 
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.5.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
 }
